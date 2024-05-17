ae1832912a3f43b8c55ba444d5fb003eca069e20
pabather <p.a.bather1@gmail.com>
2024-05-17 12:36:16 +0200

    updated production controller

--- app/controller/Production_Controller.php
+++ app/controller/Production_Controller.php
@@ -1,10 +1,26 @@
 <?php
 
-namespace controller;
+namespace app\controller;
 
+use app\model\Production_Model;
+use app\view\Production_View;
 use core\Controller;
 
 class Production_Controller extends Controller
 {
+
+    private Production_Model $production_model;
+    private Production_View $production_view;
+
+    public function __construct()
+    {
+        $this->production_model = new Production_Model();
+        $this->production_view = new Production_View();
+    }
+
+    public function renderView(): void
+    {
+        require_once $this->production_view->render();
+    }
 
 }
