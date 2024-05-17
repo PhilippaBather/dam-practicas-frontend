7654c0d4cd771961ca824cb36b37d78e865c5019
pabather <p.a.bather1@gmail.com>
2024-05-17 19:07:16 +0200

    added redirect function

--- core/Controller.php
+++ core/Controller.php
@@ -9,9 +9,21 @@ class Controller
     protected $method = DEFAULT_METHOD;     // renderView()
     protected $params = [];
 
+    const SUBFOLDER = "/dam-practicas-frontend/public/";
+
     public function getView($view, $data = []): void
     {
         require_once dirname((__DIR__)) . "../app/pages/" . $view . ".php";
+    }
+
+    public function redirect($action, $extraParams = ""): void
+    {
+        if(isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] != "off") {
+            $protocol = "https";
+        } else {
+            $protocol = "http";
+        }
+        header("Location: $protocol://" . $_SERVER["HTTP_HOST"] . Controller::SUBFOLDER . "index.php?action=". $action . $extraParams);
     }
 
 }
