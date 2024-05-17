69c1ed253059985f23a433ab6f1ee18865f76f36
pabather <p.a.bather1@gmail.com>
2024-05-17 17:31:04 +0200

    updated to handle db requests

--- app/model/Film_Model.php
+++ app/model/Film_Model.php
@@ -2,7 +2,24 @@
 
 namespace app\model;
 
+use domain\Database;
+
 class Film_Model
 {
+    private Database $db;
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getFilms(): false|array
+    {
+        return $this->db->getFilms();
+    }
+
+    public function getDirectors(): false|array
+    {
+        return $this->db->getDirectors();
+    }
 
 }
--- app/model/Production_Model.php
+++ app/model/Production_Model.php
@@ -2,7 +2,20 @@
 
 namespace app\model;
 
+use domain\Database;
+
 class Production_Model
 {
 
+    private Database $db;
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+
+    public function getProductionCompanies(): false|array
+    {
+        return $this->db->getProductionCompanies();
+    }
 }
