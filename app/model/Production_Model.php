662afe9c0e989b277861c06ffc19bfb2c65fc6fb
pabather <p.a.bather1@gmail.com>
2024-05-17 19:06:48 +0200

    updated for delete

--- app/model/Production_Model.php
+++ app/model/Production_Model.php
@@ -8,14 +8,41 @@ class Production_Model
 {
 
     private Database $db;
+    private array $errors;
     public function __construct()
     {
         $this->db = new Database();
+        $this->errors = array();
     }
 
 
     public function getProductionCompanies(): false|array
     {
         return $this->db->getProductionCompanies();
+    }
+
+    public function getSelectedCompany()
+    {
+        $company_id = $_GET['id'];
+        return $this->db->getSelectedCompany($company_id);
+    }
+
+    public function deleteCompany(): bool
+    {
+
+        // Data_Validation::unsetError(); // de-establece el array de errores
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_company'])) {
+            $company_id = $_GET['id'];
+            try {
+                $db = new Database();
+                $db->deleteSelectedCompany($company_id);
+                return true;
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+                $this->errors['post_exception'] = $e->getMessage();
+            }
+        }
+        return false;
     }
 }
