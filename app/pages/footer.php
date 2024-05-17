2a97047c818852ff43603fefd54a4d24f8172219
pabather <p.a.bather1@gmail.com>
2024-05-17 12:26:32 +0200

    added html and css

---
+++ app/pages/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <ul class="footer_container">
+        <li>
+            filmAffinity@films.co.uk
+        </li>
+        <li>
+            Instagram
+        </li>
+        <li>
+            FilmsAreUs
+        </li>
+    </ul>
+</footer>
